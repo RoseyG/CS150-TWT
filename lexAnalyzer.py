1e65a10a1bd64e19a60c0cfadab7e83c3568e674
RoseyG <mrosariotg@gmail.com>
2015-12-07 22:43:15 +0800

    Finished Lex Analyzer

--- lexAnalyzer.py
+++ lexAnalyzer.py
@@ -41,92 +41,109 @@ lesserSign = 55
 greaterSign = 56
 VARIABLE = 100
 
+def isfloat(str):
+    try:
+        float(str)
+    except ValueError:
+        return False
+    return True
 
-def lex(nextToken):
+def lex(nextString):
     if nextString == 'EOF':
-		 = -1
+        nextToken = -1
     elif nextString == '@LOGIN':
-    	nextToken = login
+        nextToken = login
     elif nextString == '@LOGOUT':
-    	nextToken = logout
-	elif nextString == '@INT':
-		nextToken = int_dec
-	elif nextString == '@COKE':
-		nextToken = float_dec
-	elif nextString == '@CHIRP':
-		nextToken = char_dec
-	elif nextString == '@MSG':
-		nextToken = string_dec
-	elif nextString == '@TRALSE':
-		nextToken = bool_dec
-	elif nextString == 'IF':
-		nextToken = if_state
+        nextToken = logout
+    elif nextString == '@INT':
+        nextToken = int_dec
+    elif nextString == '@COKE':
+        nextToken = float_dec
+    elif nextString == '@CHIRP':
+        nextToken = char_dec
+    elif nextString == '@MSG':
+        nextToken = string_dec
+    elif nextString == '@TRALSE':
+        nextToken = bool_dec
+    elif nextString == 'IF':
+        nextToken = if_state
     elif nextString == 'ELSEIF':
-    	nextToken = elseif_state
+        nextToken = elseif_state
     elif nextString == 'ELSE':
-    	nextToken = else_state
+        nextToken = else_state
     elif nextString == 'RT':
-    	nextToken = loop_state
+        nextToken = loop_state
     elif nextString == 'UNFOLLOW':
-    	nextToken = brake_state
+        nextToken = brake_state
     elif nextString == 'LIKE':
-    	nextToken = continue_state
+        nextToken = continue_state
     elif nextString == 'BLOCK':
-    	nextToken = exit_state
+        nextToken = exit_state
     elif nextString == 'FOLLOW':
-    	nextToken = exec_state
+        nextToken = exec_state
     elif nextString == 'REPLY':
-    	nextToken = read_state
+        nextToken = read_state
     elif nextString == 'TWEET':
-    	nextToken = print_state
+        nextToken = print_state
     elif nextString == 'REPORT':
-    	nextToken = return_state
+        nextToken = return_state
     elif nextString == 'YES':
-    	nextToken = TRUE
+        nextToken = TRUE
     elif nextString == 'NO':
-    	nextToken = FALSE
+        nextToken = FALSE
     elif nextString == '~':
-    	nextToken = NOT
-    elif nextString == '':
-    	nextToken = INT
-    elif nextString == '':
-    	nextToken = FLOAT
+        nextToken = NOT
+    elif nextString.isdigit():
+        nextToken = INT
+    elif nextString[0] == '-' and nextString[1:].isdigit():
+        nextToken = INT
+    elif isfloat(nextString):
+        nextToken = FLOAT
     elif (nextString[0] == '\'') and (nextString[-1] == '\''):
-    	nextToken = CHAR
+        nextToken = CHAR
     elif (nextString[0] == '\"') and (nextString[-1] == '\"'):
-    	nextToken = STRING
+        nextToken = STRING
     elif nextString == '{':
-    	nextToken = openBrace
+        nextToken = openBrace
     elif nextString == '}':
-    	nextToken = closeBrace
+        nextToken = closeBrace
     elif nextString == '+':
-    	nextToken = plusSign
+        nextToken = plusSign
     elif nextString == '-':
-    	nextToken = minusSign
+        nextToken = minusSign
     elif nextString == '/':
-    	nextToken = divSign
+        nextToken = divSign
     elif nextString == '*':
-    	nextToken = mulSign
+        nextToken = mulSign
     elif nextString == '=':
-    	nextToken = asSign
+        nextToken = asSign
     elif nextString == '(':
-    	nextToken = openParen
+        nextToken = openParen
     elif nextString == ')':
-    	nextToken = closeParen
+        nextToken = closeParen
     elif nextString == '#':
-    	nextToken = hashSymbol
+        nextToken = hashSymbol
     elif nextString == ',':
-    	nextToken = commaSign
+        nextToken = commaSign
     elif nextString == '>=':
-    	nextToken = lessEqSign
+        nextToken = lessEqSign
     elif nextString == '<=':
-    	nextToken = greatEqSign
+        nextToken = greatEqSign
     elif nextString == '==':
-    	nextToken = eqSign
+        nextToken = eqSign
     elif nextString == '>':
-    	nextToken = lesserSign
+        nextToken = lesserSign
     elif nextString == '<':
-    	nextToken = greaterSign
-    else
+        nextToken = greaterSign
+    else:
         nextToken = VARIABLE
-    return nextToken
+    print(nextToken)
+
+def main():
+    line = input("Enter: ")
+    statement = line.split()
+    for lexeme in statement:
+        lex(lexeme)
+
+if __name__ == '__main__':
+	main()
