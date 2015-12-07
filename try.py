faa6281f2f3de57d0e419d8293767659bb9abd10
gabrnavarro <gabrnavarro@gmail.com>
2015-12-07 21:36:39 +0800

    hi guys

--- try.py
+++ try.py
@@ -1,8 +1,26 @@
 ##KELLY
-dadsa
-HELLO MIKA WHAT ARE DOIN
-
-
+def Program():
+    FDefns()
+    Main()
+    
+def FDefns():
+    Declaration()
+def Main():
+    lex()
+    if (nextToken == "login")
+        Block()
+        lex()
+        if (nextToken == "logout")
+            print("Exit Main")
+            return
+        else
+            print("Error: Expected LOGOUT ")
+            exit()
+    print("Error: Expected LOGIN")
+    exit()
+def Block():
+    Exp()
+    
 ##MIKA
 lets seeee
 
