9787302af23b40ac571509b864eb4672e9792bf3
RoseyG <mrosariotg@gmail.com>
2015-12-07 20:10:24 +0800

    Started Lex Analyzer

---
+++ lexAnalyzer.py
@@ -0,0 +1,25 @@
+tralse_dec = 2
+
+
+def lex(nextToken):
+    if nextString == 'EOF':
+		 = -1
+	elif nextString == '+':
+		nextToken = 21
+	elif nextString == '-':
+		nextToken = 22
+	elif nextString == '*':
+		nextToken = 23
+	elif nextString == '/':
+		nextToken = 24
+	elif nextString == '(':
+		nextToken = 25
+	elif nextString == ')':
+		nextToken = 26
+	elif nextString == 'a':
+		nextToken = 11
+	elif nextString == 'b':
+		nextToken = 11
+	elif nextString == 'c':
+		nextToken = 11
+    return nextToken
