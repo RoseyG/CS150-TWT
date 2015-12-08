9cb30f3d116a76631eb72e58e9ebb022470a5e6b
Jahziel Rae Arceo <rae.arceo@live.com>
2015-12-08 22:39:44 +0800

    Added lines for lex on parser code

--- try.py
+++ try.py
@@ -42,6 +42,104 @@ lesserSign = 55
 greaterSign = 56
 VARIABLE = 100
 
+def isfloat(str):
+    try:
+        float(str)
+    except ValueError:
+        return False
+    return True
+
+def lex():
+    if nextString == 'EOF':
+        nextToken = -1
+    elif nextString == '@LOGIN':
+        nextToken = login
+    elif nextString == '@LOGOUT':
+        nextToken = logout
+    elif nextString == '@INT':
+        nextToken = int_dec
+    elif nextString == '@COKE':
+        nextToken = float_dec
+    elif nextString == '@CHIRP':
+        nextToken = char_dec
+    elif nextString == '@MSG':
+        nextToken = string_dec
+    elif nextString == '@TRALSE':
+        nextToken = bool_dec
+    elif nextString == 'IF':
+        nextToken = if_state
+    elif nextString == 'ELSEIF':
+        nextToken = elseif_state
+    elif nextString == 'ELSE':
+        nextToken = else_state
+    elif nextString == 'RT':
+        nextToken = loop_state
+    elif nextString == 'UNFOLLOW':
+        nextToken = brake_state
+    elif nextString == 'LIKE':
+        nextToken = continue_state
+    elif nextString == 'BLOCK':
+        nextToken = exit_state
+    elif nextString == 'FOLLOW':
+        nextToken = exec_state
+    elif nextString == 'REPLY':
+        nextToken = read_state
+    elif nextString == 'TWEET':
+        nextToken = print_state
+    elif nextString == 'REPORT':
+        nextToken = return_state
+    elif nextString == 'YES':
+        nextToken = TRUE
+    elif nextString == 'NO':
+        nextToken = FALSE
+    elif nextString == '~':
+        nextToken = NOT
+    elif nextString.isdigit():
+        nextToken = INT
+    elif nextString[0] == '-' and nextString[1:].isdigit():
+        nextToken = INT
+    elif isfloat(nextString):
+        nextToken = FLOAT
+    elif (nextString[0] == '\'') and (nextString[-1] == '\''):
+        nextToken = CHAR
+    elif (nextString[0] == '\"') and (nextString[-1] == '\"'):
+        nextToken = STRING
+    elif nextString == '{':
+        nextToken = openBrace
+    elif nextString == '}':
+        nextToken = closeBrace
+    elif nextString == '+':
+        nextToken = plusSign
+    elif nextString == '-':
+        nextToken = minusSign
+    elif nextString == '/':
+        nextToken = divSign
+    elif nextString == '*':
+        nextToken = mulSign
+    elif nextString == '=':
+        nextToken = asSign
+    elif nextString == '(':
+        nextToken = openParen
+    elif nextString == ')':
+        nextToken = closeParen
+    elif nextString == '#':
+        nextToken = hashSymbol
+    elif nextString == ',':
+        nextToken = commaSign
+    elif nextString == '>=':
+        nextToken = lessEqSign
+    elif nextString == '<=':
+        nextToken = greatEqSign
+    elif nextString == '==':
+        nextToken = eqSign
+    elif nextString == '>':
+        nextToken = lesserSign
+    elif nextString == '<':
+        nextToken = greaterSign
+    else:
+        nextToken = VARIABLE
+    print(nextToken)
+
 ##KELLY
 def Program():
     FDefns()
